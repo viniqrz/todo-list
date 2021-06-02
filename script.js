2f149848ab4301b31c02b1f795b4a8c052f2cb86
Vinicius <viniderp@gmail.com>
2021-06-02 16:49:31 -0300

    8-11 Requirements

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const btnAdd = document.querySelector('#criar-tarefa');
 const inputAdd = document.querySelector('#texto-tarefa');
+const btnRemoveAll = document.querySelector('#apaga-tudo');
+const btnRemoveCompleted = document.querySelector('#remover-finalizados');
 const list = document.querySelector('#lista-tarefas');
+
+let highlighted = false;
 
 btnAdd.addEventListener('click', function(e) {
   e.preventDefault();
@@ -8,8 +12,33 @@ btnAdd.addEventListener('click', function(e) {
 
   list.insertAdjacentHTML('beforeend', `<li>${inputAdd.value}</li>`);
   inputAdd.value = '';
+});
+
+btnRemoveAll.addEventListener('click', function(e) {
+  list.innerHTML = '';
+});
+
+btnRemoveCompleted.addEventListener('click', function(e) {
+  const completed = document.querySelectorAll('.completed');
+  
+  if (completed) {
+    for (const el of completed) el.remove();
+  }
 })
 
 list.addEventListener('click', function(e) {
-  e.target.classList.add('highlight');
-})
+  if (highlighted) highlighted.classList.remove('highlight');
+
+  highlighted = e.target;
+
+  highlighted.classList.add('highlight');
+});
+
+list.addEventListener('dblclick', function(e) {
+  if (e.target.classList.contains('completed')) {
+    e.target.classList.remove('completed');
+    return;
+  }
+
+  e.target.classList.add('completed');
+});
