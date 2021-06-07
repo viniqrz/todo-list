f29f76cf0b963ecd73e6cfcb6fc07957c503706f
Vinicius <viniderp@gmail.com>
2021-06-07 16:20:30 -0300

    Change Overall Style

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-const btnAdd = document.querySelector('#criar-tarefa');
 const inputAdd = document.querySelector('#texto-tarefa');
-const btnRemoveAll = document.querySelector('#apaga-tudo');
-const btnRemoveCompleted = document.querySelector('#remover-finalizados');
+
+const btnAdd = document.querySelector('#criar-tarefa');
 const btnSaveList = document.querySelector('#salvar-tarefas');
 const btnMoveUp = document.querySelector('#mover-cima');
+const btnRemoveAll = document.querySelector('#apaga-tudo');
+const btnRemoveCompleted = document.querySelector('#remover-finalizados');
 const btnMoveDown = document.querySelector('#mover-baixo');
 const btnRemoveSelected = document.querySelector('#remover-selecionado');
+
 const list = document.querySelector('#lista-tarefas');
 
-let highlighted = false;
-let listData = {
-  items: [],
-};
+let highlighted = null;
 
-btnAdd.addEventListener('click', function(e) {
+btnAdd.addEventListener('click', function (e) {
   e.preventDefault();
   if (inputAdd.value.trim().length < 3) return;
 
@@ -21,19 +20,19 @@ btnAdd.addEventListener('click', function(e) {
   inputAdd.value = '';
 });
 
-btnRemoveAll.addEventListener('click', function(e) {
+btnRemoveAll.addEventListener('click', function () {
   list.innerHTML = '';
 });
 
-btnRemoveCompleted.addEventListener('click', function(e) {
+btnRemoveCompleted.addEventListener('click', function () {
   const completed = document.querySelectorAll('.completed');
-  
+
   if (completed) {
     for (const el of completed) el.remove();
   }
-})
+});
 
-list.addEventListener('click', function(e) {
+list.addEventListener('click', function (e) {
   if (highlighted) highlighted.classList.remove('highlight');
 
   highlighted = e.target;
@@ -41,7 +40,7 @@ list.addEventListener('click', function(e) {
   highlighted.classList.add('highlight');
 });
 
-list.addEventListener('dblclick', function(e) {
+list.addEventListener('dblclick', function (e) {
   if (e.target.classList.contains('completed')) {
     e.target.classList.remove('completed');
     return;
@@ -50,55 +49,56 @@ list.addEventListener('dblclick', function(e) {
   e.target.classList.add('completed');
 });
 
-btnSaveList.addEventListener('click', function(e) {
+btnSaveList.addEventListener('click', function (e) {
   if (highlighted) highlighted.classList.remove('highlight');
 
   const currentList = document.querySelector('#lista-tarefas');
 
   localStorage.setItem('listItems', JSON.stringify(currentList.innerHTML));
-})
+});
 
-btnMoveUp.addEventListener('click', function(e) {
+btnMoveUp.addEventListener('click', function (e) {
   const currentList = document.querySelector('#lista-tarefas');
-  let arr = Array.prototype.slice.call(currentList.childNodes);
-
+  let arr = Array.prototype.slice.call(currentList.children);
   if (!highlighted) return;
+
   if (arr.indexOf(highlighted) === 0) return;
 
   arr = [
-    ...arr.slice(0, arr.indexOf(highlighted) - 1), 
+    ...arr.slice(0, arr.indexOf(highlighted) - 1),
     highlighted,
-    arr[arr.indexOf(highlighted) - 1], 
-    ...arr.slice(arr.indexOf(highlighted)+1)
+    arr[arr.indexOf(highlighted) - 1],
+    ...arr.slice(arr.indexOf(highlighted) + 1)
   ];
 
   currentList.innerHTML = '';
   arr.forEach(el => currentList.appendChild(el));
-})
+});
 
-btnMoveDown.addEventListener('click', function(e) {
+btnMoveDown.addEventListener('click', function (e) {
   const currentList = document.querySelector('#lista-tarefas');
-  let arr = Array.prototype.slice.call(currentList.childNodes);
-
+  let arr = Array.prototype.slice.call(currentList.children);
   if (!highlighted) return;
+
   if (arr.indexOf(highlighted) === arr.length - 1) return;
 
   arr = [
-    ...arr.slice(0, arr.indexOf(highlighted)), 
+    ...arr.slice(0, arr.indexOf(highlighted)),
     arr[arr.indexOf(highlighted) + 1],
-    highlighted, 
-    ...arr.slice(arr.indexOf(highlighted)+2)
+    highlighted,
+    ...arr.slice(arr.indexOf(highlighted) + 2)
   ];
+
 
   currentList.innerHTML = '';
   arr.forEach(el => currentList.appendChild(el));
-})
+});
 
-btnRemoveSelected.addEventListener('click', function(e) {
+btnRemoveSelected.addEventListener('click', function (e) {
   const currentList = document.querySelector('#lista-tarefas');
-  
+
   if (highlighted) currentList.removeChild(highlighted);
-})
+});
 
 
 if (localStorage.getItem('listItems')) {
